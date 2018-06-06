a74843a11fd950d2b48eaa8b48d31c049d7d090c
Kyle <kylesclawson@gmail.com>
2018-06-05 18:53:47 -0600

    more on the big quote

--- src/Components/BigQuote/BigQuote.js
+++ src/Components/BigQuote/BigQuote.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import FaLongArrowRight from 'react-icons/lib/fa/long-arrow-right';
 
 function BigQuote(props){
     const {quote, name, title, link} = props.bigQuote;
     return(
-        <div>
-            <a href={link}><p>{quote}</p></a>
-            <p>{title} {name} IN <a href='https://thecreativeindependent.com/?ref=ksr_home'>THE CREATIVE INDEPENDENT</a>, A KICKSTARTER-PUBLISHED RESOURCE FOR CREATIVE PEOPLE</p>
+        <div className='big-quote'>
+            <a  className = 'big-quote-pic' href={link}>
+                <img className = 'big-quote-pic'src='https://d3mlfyygrfdi2i.cloudfront.net/tci-swirl.png'/>
+            </a>
+            <a className='big-quote-quote' href={link}>
+                <h1 className='big-quote-quote'>{quote}</h1>
+            </a>
+            <p className = 'big-quote-signature'>{title} {name} IN <a href='https://thecreativeindependent.com/?ref=ksr_home'>THE CREATIVE INDEPENDENT</a>, A KICKSTARTER-PUBLISHED RESOURCE FOR CREATIVE PEOPLE</p>
+            <a href={link} className='big-quote-read'><p>READ MORE</p> <FaLongArrowRight/></a>
         </div>
     )
 }
