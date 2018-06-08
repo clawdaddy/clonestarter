0dfa0cba51f3203b0b46dbbb17300d4277a807ef
Kyle <kylesclawson@gmail.com>
2018-06-08 11:27:36 -0600

    get closer on the cards

--- src/Components/BannerLinks/BannerLinks.js
+++ src/Components/BannerLinks/BannerLinks.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import './../Splash/Splash.css';
 
 function BannerLinks (props){
     return(
-        <div>BannerLinks</div>
+        <div className='banners'>
+            <div className='go-green'>
+                <span className='green-header'>Go Green</span>
+                <span className='green-sentence'>Discover projects helping to build a more sustainable future.</span>
+            </div>
+            <div className='drip'>
+                <div>
+                    <svg className='drop-svg'xmlns='http://www.w3.org/2000/svg'/>
+                </div>
+            </div>
+        </div>
     )
 }
 export default BannerLinks;
