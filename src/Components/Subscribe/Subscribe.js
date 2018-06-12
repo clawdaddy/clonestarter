ebfdeb1b0ae317eae85863ebf60558102c8316a7
Kyle <kylesclawson@gmail.com>
2018-06-12 14:43:52 -0600

    touch ups

--- src/Components/Subscribe/Subscribe.js
+++ src/Components/Subscribe/Subscribe.js
@@ -10,8 +10,11 @@ class Subscribe extends Component {
 
     render(){
         return(
-            <div>
-                Subscribe
+            <div className='subscribe'>
+                <h3 className='subscribe-header'>Subscribe to our weekly newsletter</h3>
+                <p className='subscribe-sentence'>Sign up for Projects We Love to receive a weekly mix of noteworthy projects, handpicked by our team.</p>
+                <input placeholder='Enter email address' className='subscribe-input'/>
+                <button className='subscribe-button'>Subscribe now</button>
             </div>
         )
     }
